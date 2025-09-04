1738b010a68bdd327c79b918516129240ed237f1
Leon <leonlaf66@gmail.com>
2025-09-04 10:47:12 -0400

    deploy prod

---
+++ prod/main.tf
@@ -0,0 +1,21 @@
+module "spacelift_demo" {
+  source   = "../common_modules/ec2"
+  app_name = "spacelift-demo"
+  common_tags = {
+    Owner       = "LEON"
+    Project     = "Spacelift Demo"
+    ManagedBy   = "Terraform"
+    Environment = "dev"
+  }
+
+  ingress_rule = [
+    {
+      description = "Allow SSH from my IP"
+      from_port   = 22
+      to_port     = 22
+      ip_protocol = "tcp"
+      cidr_ipv4   = "100.0.19.192/32"
+      referenced_security_group_id = null
+    }
+  ]
+}
