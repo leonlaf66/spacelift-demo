bc74eb94a644ded5313bf9882eb13e45fc926290
Leon <leonlaf66@gmail.com>
2025-09-10 16:56:17 -0400

    test

--- dev/main.tf
+++ dev/main.tf
@@ -6,7 +6,7 @@ module "spacelift_demo" {
   ingress_rule = [
     {
       description = "Allow SSH from my IP"
-      from_port   = 22
+      from_port   = 21
       to_port     = 22
       ip_protocol = "tcp"
       cidr_ipv4   = "100.0.19.192/32"
